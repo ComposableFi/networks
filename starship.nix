6d9ed081cd17055d84b9110955e94593a999202c
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-01-04 15:00:25 +0000

    Update networks client setup

--- starship.nix
+++ starship.nix
@@ -1,2 +1,3 @@
 # - common modules for clis (jq/dasel/base32cli/etc), never speific for specific chain
 # - common bash scripts for cosmos sdk/polkadot chains init/gen/run/clean up/teardown
+# - sure NO process manager integration/modules
