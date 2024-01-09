2c613e032467945ce3d430fb32706383810d9c7f
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-01-09 12:21:38 +0100

    Update networks client setup

--- starship.nix
+++ starship.nix
@@ -1,9 +1,8 @@
 # - common modules for clis (jq/dasel/base32cli/etc), never speific for specific chain
 # - common bash scripts for cosmos sdk/polkadot chains init/gen/run/clean up/teardown
 # - sure NO process manager integration/modules
-
-              # dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value  "test" "keyring-backend"
-              # dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value "json" "output" 
-              # dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value "${env.NODE}" "node" 
-              # dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value  "${env.CHAIN_ID}" "chain-id"
-              # dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value  "sync" "broadcast-mode"
+# dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value  "test" "keyring-backend"
+# dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value "json" "output" 
+# dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value "${env.NODE}" "node" 
+# dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value  "${env.CHAIN_ID}" "chain-id"
+# dasel put --type=string --write=toml --file="$CONFIG_FOLDER"/client.toml --value  "sync" "broadcast-mode"
